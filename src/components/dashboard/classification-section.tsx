a359b58019047d88b4d07d79bc00c3b27fa69d42
georgebuma.jb <georgebuma.jb@gmail.com>
2026-04-12 07:42:47 +0000

    charts renew

--- src/components/dashboard/classification-section.tsx
+++ src/components/dashboard/classification-section.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { cn } from '@/lib/utils';
 import * as React from 'react';
